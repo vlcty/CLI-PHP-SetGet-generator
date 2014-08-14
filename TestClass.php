08e449512f908e17133b3e3e302d5b5b6a7aee66
Josef <hello@veloc1ty.de>
2014-08-14 12:25:00 +0200

    Added simple test class for fast tryout

---
+++ TestClass.php
@@ -0,0 +1,25 @@
+<?php
+class Flame {
+	# Integers
+	private $int1 = 0;
+	protected $int2 = 0; # Comments begind something should not generate errors
+
+	# Strings
+	private $string1 = "";
+	private $string2 = '';
+	protected $string3 = "";
+	protected $string4 = '';
+
+	# Objects
+	private $object1 = null;
+	protected $object2 = null;
+
+	# Float
+	private $float1 = 0.0;
+	protected $float2 = 0.0;
+
+	# Array
+	private $array1 = array();
+	protected $array2 = array();
+}
+?>
